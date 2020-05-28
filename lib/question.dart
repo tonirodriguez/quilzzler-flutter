bd40c1806acfc579824178b03706e35f794b6868
tonirodriguez <arodriguezt@gmail.com>
2020-05-28 15:38:29 +0200

    Optimization

--- lib/question.dart
+++ lib/question.dart
@@ -2,10 +2,5 @@ class Question {
   String questionText;
   bool questionAnswer;
 
-  Question(String q, bool a) {
-    questionText = q;
-    questionAnswer = a;
-  }
+  Question(this.questionText, this.questionAnswer);
 }
-
-Question newQuestion = Question('text', true);
